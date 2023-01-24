cdee72b37c036dbf6e78ac955aa216117a19b14a
daniel-alencar-br <danielalencar.br@gmail.com>
2023-01-23 21:08:06 -0300

    Adicionar arquivos de projeto.

---
+++ Aula04Atos/Controllers/ClientesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Aula04Atos.Models;
+using System.Data;
+
+namespace Aula04Atos.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public ActionResult Index()
+        {
+            Repositorio Pesq = new Repositorio();
+            DataSet Tabelas;
+            // 1..N : DataTable  1..N : DataRow
+
+            Tabelas = Pesq.VoltaClientes();
+
+            IEnumerable<DataRow> Clientes =
+                from Cli in Tabelas.Tables[0].AsEnumerable()
+                  orderby Cli.Field<int>("Idade") descending
+                    select Cli;
+
+            IEnumerable<DataRow> ClientesMais40 =
+                 Clientes.Where(p => p.Field<int>("Idade") > 40);
+
+            int iTotal = Clientes.Sum(p => p.Field<int>("Idade"));
+
+            ViewBag.Dados = "";
+            foreach (DataRow Linha in Clientes)
+            {
+                ViewBag.Dados += Linha["Nome"].ToString() + " - ";
+            }
+
+            // Varredura Total
+            /*ViewBag.Dados = "";
+            foreach (DataRow Linha in Tabelas.Tables[0].Rows)
+            {
+                ViewBag.Dados += Linha["Nome"].ToString() + " - ";
+            }*/
+
+            return View();
+        }
+    }
+}
+
---
+++ Aula04Atos/Models/Repositorio.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Aula04Atos.Models
+{
+    public class Repositorio
+    {
+        SqlConnection CN = new SqlConnection();    // conexão
+        SqlCommand CM = new SqlCommand();          // comando a executar
+        SqlDataAdapter DA = new SqlDataAdapter();  // executa o comando
+
+        DataSet DS = new DataSet();  // Recebe a Tabela e a mantem offline
+
+        public DataSet VoltaClientes()
+        {
+            // Apenas se OleDb :  "Provider=MSSQLSERVER;"
+
+            // servidor, banco de dados,
+            // usuário e senha(sql) OU usuário do windows
+            CN.ConnectionString = "data source=pc_do_gab;" + 
+                                  "initial catalog=CursoAsp;" + 
+                                  "User Id=sa; Password=sa";
+                                // OU
+                                // "Integrated Security=SSPI"
+            
+            // Se vincula a conexão e define o comando
+            CM.Connection = CN;
+            CM.CommandText = "Select * From Clientes";
+
+            // Se vincula ao comando
+            DA.SelectCommand = CM;
+
+            CN.Open();
+
+            // DS.Tables[0].Rows[0]["Nome"].ToString();
+            
+            // Tipos de Execução
+            DA.Fill(DS);   // receber uma tabela
+
+            //CM.ExecuteNonQuery();   // executa sem retorno (I U D)
+            //CM.ExecuteScalar();     // executa e retorna UM valor
+            //CM.ExecuteReader();     // retorna DataReader (conexão aberta)
+ 
+            CN.Close();
+            return DS;
+        }
+
+    }
+}
